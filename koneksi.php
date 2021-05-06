3bcfd5dee1d2262cbf97f9a2e0dc0eeed7025d38
Agus Indra <agusindra376@gmail.com>
2021-05-06 23:01:38 +0800

    Revisi 4

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,11 @@
 <?php
-$host        = "ec2-18-233-83-165.compute-1.amazonaws.com";
-$port        = "port = 5432";
-$dbname      = "dbname = dcmgivdkils91u";
-$credentials = "user = ntmzeddiidsezd
- password= e395ceec2d7a2be3c3e18597213cb399227fd12875b3beb3f288f7ae5e3a166d";
+$host         = "ec2-18-233-83-165.compute-1.amazonaws.com";
+$port         = "port = 5432";
+$dbname       = "dbname = dcmgivdkils91u";
+$un           = "user = ntmzeddiidsezd";
+$ps           = "password= e395ceec2d7a2be3c3e18597213cb399227fd12875b3beb3f288f7ae5e3a166d";
 
-$conn = mysqli_connect("$host", "$port", "$dbname ", "$credentials");
+$conn = mysqli_connect("$host", "$port","$dbname", "$un", "$ps");
 
 //query
 function query($query){
@@ -91,13 +91,6 @@ function hapus($id)
   return mysqli_affected_rows($conn);
 }
 
-//upload gambar
-function upload(){
-  $namaFile = $_FILES['gambar']['name'];
-  $ukuranFile = $_FILES['gambar']['size'];
-  $error = $_FILES['gambar']['error'];
-  $tmpName = $_FILES['gambar']['tmp_name'];
-
   //cek apakah tidak ada gambar yg diupload
 if ( $error === 4) {
     echo "<script>
@@ -105,34 +98,6 @@ if ( $error === 4) {
       </script>";
     return false;
   }
-
-  //cek apakah yang diupload adalah gambar
-//   $ekstensiGambarValid = ['jpg','jpeg','png'];
-//   $ekstensiGambar = explode('.', $namaFile);
-//   $ekstensiGambar = strtolower(end($ekstensiGambar));
-//   if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
-//     echo "<script>
-//         alert('Yang diupload bukan berekstensi gambar!');
-//       </script>";
-//     return false;
-//   }
-
-  //cek jika ukuran terlalu besar
-//   if ($ukuranFile > 5000000) {
-//     echo "<script>
-//         alert('Ukuran gambar terlalu besar!');
-//       </script>";
-//     return false;
-//   }
-
-//   //lolos pengecekan, gambar siap diupload
-//   // generate nama gambar baru
-//   $namaFileBaru = uniqid();
-//   $namaFileBaru.= '.';
-//   $namaFileBaru.= $ekstensiGambar;
-//   move_uploaded_file($tmpName, 'img/'.$namaFileBaru);
-//   return $namaFileBaru;
-// }
 
 //ubah data
 function ubah($data)
